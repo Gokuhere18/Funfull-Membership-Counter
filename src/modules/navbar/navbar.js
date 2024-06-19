dd97fd97be5dec27d6572905d1e510ebae542174
shivam <shivam@funfull.com>
2024-06-19 21:42:53 +0530

    comit

--- src/modules/navbar/navbar.js
+++ src/modules/navbar/navbar.js
@@ -4,8 +4,8 @@ import './navbar.css';
 function Navbar({ onComponentChange }) {
   return (
     <nav className="navbar">
-      <button onClick={() => onComponentChange('MembershipCounter')}>Membership Counter</button>
-      <button onClick={() => onComponentChange('PassCounter')}>SummerPass Counter</button>
+      <button onClick={() => onComponentChange('MembershipCounter')}>Active Memberships</button>
+      <button onClick={() => onComponentChange('PassCounter')}>Pass Counter</button>
     </nav>
   );
 }
